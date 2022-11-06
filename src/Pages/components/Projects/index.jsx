cf79488a845472e670ce5cc66dc3bfe2e97908fa
oliveiramiro2 <ramiromares95@gmail.com>
2022-11-06 16:54:44 -0300

    start to add the appetize.io to show react native app into web site

--- src/Pages/components/Projects/index.jsx
+++ src/Pages/components/Projects/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Netflix from '../../../Assets/netflix.png';
+import Breve from '../../../Assets/breve.png';
 
 import './index.css';
 
@@ -7,9 +9,22 @@ const Projects = () => (
     <div>
       <p className="textProjects">Projetos</p>
     </div>
-    {/* <div>
-
-    </div> */}
+    <div className="containProjectsImgs">
+      <img className="imgsProject" src={Netflix} alt="Netflix" />
+      <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" />
+      <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" />
+      <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" />
+      <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" />
+      <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" />
+      <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" />
+    </div>
+    <div>
+      <iframe
+        id="appetize"
+        src="https://appetize.io/app/myzsjsp6sqeyr4rmceemurm3e4"
+        title="netflix"
+      />
+    </div>
   </div>
 );
 
