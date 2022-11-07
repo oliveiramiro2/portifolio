a1becf389f71a757f196494b23d767a73912c27a
oliveiramiro2 <ramiromares95@gmail.com>
2022-11-07 17:37:28 -0300

    add links in the header to my github and linkedin

--- src/Pages/components/Header/index.jsx
+++ src/Pages/components/Header/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
 import './index.css';
 
 const Header = () => (
   <header className="contain">
-    <div className="containText">
-      <p className="text">Ramiro Mares de Oliveira</p>
+    <div className="containIconsLeft">
+      <a href="https://github.com/oliveiramiro2" target="_blank" rel="noreferrer">
+        <AiFillGithub color="#fff" size={25} />
+        GitHub
+      </a>
+      <a href="https://www.linkedin.com/in/oliveiramiro2" target="_blank" rel="noreferrer">
+        <AiFillLinkedin color="#fff" size={25} />
+        Linkedin
+      </a>
     </div>
     <div className="containTextRight">
       <p className="text">Sobre mim</p>
