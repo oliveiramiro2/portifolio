fca5fc05d60b8dff1b286a2e0653666d965fbcd2
oliveiramiro2 <ramiromares95@gmail.com>
2022-11-08 02:33:58 -0300

    test

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import App from './src/Pages/App';
-import reportWebVitals from './src/reportWebVitals';
+import App from './Pages/App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
