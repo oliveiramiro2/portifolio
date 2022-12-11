ef9785be97722148aa82c382ff8f2c6304288087
oliveiramiro2 <ramiromares95@gmail.com>
2022-12-11 07:16:10 -0300

    start build the footer

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutUs from '../components/AboutUs';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
@@ -12,6 +13,7 @@ const Home = () => (
     <AboutUs />
     <Skills />
     <Projects />
+    <Footer />
   </div>
 );
 
---
+++ src/Pages/components/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './index.css';
+
+const Footer = () => (
+  <footer>
+    <div>
+      <p className="textFooter">Email</p>
+      <span className="textFooter">ramiromares95@gmail.com</span>
+    </div>
+    <div>
+      <p className="textFooter">Telefone/whatsapp</p>
+      <span className="textFooter">(37) 98803-7773</span>
+    </div>
+  </footer>
+);
+
+export default Footer;
