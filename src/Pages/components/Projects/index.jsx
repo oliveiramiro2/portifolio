5205d73918a3da77dab41eb35a6d22616f89cabb
oliveiramiro2 <ramiromares95@gmail.com>
2022-12-14 19:34:48 -0300

    added pokédex project and adjust on this project

--- src/Pages/components/Projects/index.jsx
+++ src/Pages/components/Projects/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
 import { BiArrowBack } from 'react-icons/bi';
+
 import Netflix from '../../../Assets/netflix.png';
+import Pokedex from '../../../Assets/Pokedex.png';
 import Breve from '../../../Assets/breve.png';
 
 import './index.css';
@@ -10,7 +12,16 @@ const projectsKeys = [
   {
     id: 0,
     value: 'l5vwa3gsaqewdnjkuw6a5hx62u',
+    gitRepository: 'https://github.com/oliveiramiro2/NetflixClone',
     describe: 'Um clone do aplicativo Netflix usando React-Native',
+    mobile: true,
+  },
+  {
+    id: 1,
+    value: 'https://pokedex-ramiro.netlify.app/',
+    gitRepository: 'https://github.com/oliveiramiro2/pokedex-updated',
+    describe: 'Uma pokédex criada com ReactJs e Typescript, além de libs como Styled Components e Material UI',
+    mobile: false,
   },
 ];
 
@@ -28,46 +39,32 @@ const Projects = () => {
         ? (
           <div className="containProjectsImgs">
             <div className="containProject">
-              <ScrollAnimation animateIn="fadeIn" animateOut="fadeOuv" className="containProject">
                 <p className="textDescribe">Netflix</p>
                 <img className="imgsProject" src={Netflix} alt="Netflix" onClick={() => setNumberProjectShowed(0)} width={100} height={100} />
-              </ScrollAnimation>
             </div>
             <div className="containProject">
-              <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" className="containProject">
-                <p className="textDescribe">Em Breve</p>
-                <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" width={100} height={100} />
-              </ScrollAnimation>
+                <p className="textDescribe">Pokédex</p>
+                <img className="imgsProject" src={Pokedex} alt="Pokedex" onClick={() => setNumberProjectShowed(1)} width={100} height={100} />
             </div>
             <div className="containProject">
-              <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" className="containProject">
                 <p className="textDescribe">Em Breve</p>
                 <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" width={100} height={100} />
-              </ScrollAnimation>
             </div>
             <div className="containProject">
-              <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" className="containProject">
                 <p className="textDescribe">Em Breve</p>
                 <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" width={100} height={100} />
-              </ScrollAnimation>
             </div>
             <div className="containProject">
-              <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" className="containProject">
                 <p className="textDescribe">Em Breve</p>
                 <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" width={100} height={100} />
-              </ScrollAnimation>
             </div>
             <div className="containProject">
-              <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" className="containProject">
                 <p className="textDescribe">Em Breve</p>
                 <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" width={100} height={100} />
-              </ScrollAnimation>
             </div>
             <div className="containProject">
-              <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" className="containProject">
                 <p className="textDescribe">Em Breve</p>
                 <img className="imgsProject imgsProjectsEmpty" src={Breve} alt="Breve" width={100} height={100} />
-              </ScrollAnimation>
             </div>
           </div>
         )
@@ -82,19 +79,24 @@ const Projects = () => {
                 </p>
               </div>
               <p className="textDescribe">{projectsKeys[numberProjectShowed].describe}</p>
-              <p className="textDescribe">Uma prévia será aberta com o appetize a direita da tela!</p>
+              {projectsKeys[numberProjectShowed].mobile
+                ? <p className="textDescribe">Uma prévia será aberta com o appetize a direita da tela!</p>
+                : (<p className="textDescribe">
+                    <a href={projectsKeys[numberProjectShowed].value} target="_blank" rel="noreferrer">O Link do site</a>
+                  </p>)}
               <p className="textDescribe">Aprecie :D</p>
-              <a href="https://github.com/oliveiramiro2/NetflixClone" target="_blank" rel="noreferrer">Repositório no GitHub</a>
+              <a href={projectsKeys[numberProjectShowed].gitRepository} target="_blank" rel="noreferrer">Repositório no GitHub</a>
             </div>
-            <iframe
-              id="appetize"
-              src={`https://appetize.io/embed/${projectsKeys[numberProjectShowed].value}?device=pixel4`}
-              title="netflix"
-              height="700px"
-              width="320px"
-              frameBorder="0"
-              scrolling="no"
-            />
+            {projectsKeys[numberProjectShowed].mobile
+            && <iframe
+                    id="appetize"
+                    src={`https://appetize.io/embed/${projectsKeys[numberProjectShowed].value}?device=pixel4`}
+                    title="netflix"
+                    height="700px"
+                    width="320px"
+                    frameBorder="0"
+                    scrolling="no"
+                />}
           </div>
         )}
     </div>
